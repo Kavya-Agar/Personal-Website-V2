9081274290dbaab470aea8c50b9885fa88539dec
Kavya-Agar <kavyaagar0@gmail.com>
2025-08-13 02:02:55 -0500

    Removed unnecessary imports, removed dark mode toggle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,38 +8,33 @@ import Experience from "./components/Experience.jsx";
 import Projects from "./components/Projects.jsx";
 import Footer from "./components/Footer.jsx";
 import DarkVeil from "./blocks/Backgrounds/DarkVeil/DarkVeil.jsx";
-import Aurora from "./blocks/Backgrounds/Aurora/Aurora.jsx";
-import CustomCursor from "./components/Cursor.jsx";
 
 function AppContent() {
-  const [darkMode, setDarkMode] = useState(false);
 
   return (
     <div className="min-h-screen">
       <div className="fixed inset-0 -z-10">
-        {!darkMode && <DarkVeil speed={2} hueShift={60} warpAmount={5} />}
-        {darkMode && <Aurora colorStops={["#DF1125", "#FFFFFF", "#5227FF"]} blend={0.5} amplitude={1.0} speed={1.5} />}
+        <DarkVeil speed={2} hueShift={60} warpAmount={5} />
       </div>
 
-        <div className={`flex flex-col min-h-screen ${darkMode ? "cursor-none" : ""}`}>
-        <Navbar enabled={darkMode} setEnabled={setDarkMode} />
-        {darkMode && <CustomCursor/>}
+        <div className="flex flex-col min-h-screen">
+        <Navbar  />
         <Routes>
           <Route
             path="/"
-            element={<Home enabled={darkMode} setEnabled={setDarkMode} />}
+            element={<Home />}
           />
           <Route
             path="/about"
-            element={<About enabled={darkMode} setEnabled={setDarkMode} />}
+            element={<About />}
           />
           <Route
             path="/experience"
-            element={<Experience enabled={darkMode} setEnabled={setDarkMode} />}
+            element={<Experience />}
           />
           <Route
             path="/projects"
-            element={<Projects enabled={darkMode} setEnabled={setDarkMode} />}
+            element={<Projects />}
           />
         </Routes>
         <Footer />
