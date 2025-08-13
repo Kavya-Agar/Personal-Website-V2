f9b49b427b1966e300ac2e149f18aadf031ca616
Kavya-Agar <kavyaagar0@gmail.com>
2025-08-13 02:09:23 -0500

    added instagram link to footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
-import { FiGithub } from "react-icons/fi";
+import { FiGithub, FiInstagram } from "react-icons/fi";
 import { FaLinkedinIn } from "react-icons/fa";
 import { GrDocumentUser } from "react-icons/gr";
 
-export const FooterBar = ({ enabled }) => {
-  const textColor = enabled ? "text-amber-400" : "text-amber-100";
+export const FooterBar = () => {;
 
   return (
-    <footer className={`w-full ${textColor} py-4 flex flex-col bg-transparent backdrop-filter backdrop-blur-2xl items-center justify-center`}>
-      <div className={`text-sm mt-1 opacity-70 lato-black ${textColor}`}>
+    <footer
+      className={`w-full text-amber-100 py-4 flex flex-col bg-transparent backdrop-filter backdrop-blur-2xl items-center justify-center`}
+    >
+      <div className="text-sm mt-1 opacity-70 lato-black">
         © {new Date().getFullYear()} Kavya Agar. All rights reserved.
       </div>
       <div className="flex space-x-4 mt-2">
@@ -15,7 +16,7 @@ export const FooterBar = ({ enabled }) => {
           href="https://github.com/Kavya-Agar"
           target="_blank"
           rel="noopener noreferrer"
-          className={`transition hover:text-amber-400 ${textColor}`}
+          className="transition hover:text-amber-400"
         >
           <FiGithub />
         </a>
@@ -23,7 +24,7 @@ export const FooterBar = ({ enabled }) => {
           href="https://www.linkedin.com/in/kavya-agar/"
           target="_blank"
           rel="noopener noreferrer"
-          className={`transition hover:text-amber-400 ${textColor}`}
+          className="transition hover:text-amber-400"
         >
           <FaLinkedinIn />
         </a>
@@ -31,9 +32,17 @@ export const FooterBar = ({ enabled }) => {
           href="https://drive.google.com/file/d/1yjXL089_K-2ya0a3UqECIs09KQ6Z97tW/view?usp=drive_link"
           target="_blank"
           rel="noopener noreferrer"
-          className={`transition hover:text-amber-400 ${textColor}`}
+          className="transition hover:text-amber-400"
         >
           <GrDocumentUser />
+        </a>
+        <a
+          href="https://www.instagram.com/kavyaagar.ig/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`flex items-center text-amber-100 hover:text-amber-400 `}
+        >
+          <FiInstagram />
         </a>
       </div>
     </footer>
