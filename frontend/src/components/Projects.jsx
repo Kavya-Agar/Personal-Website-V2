0c90567a41e428891e68bb0c222a27d7219a79d5
Kavya-Agar <kavyaagar0@gmail.com>
2025-08-14 01:46:53 -0500

    Updates

--- frontend/src/components/Projects.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ProjectList } from "./Projects/Project List";
-
-export default function Projects({enabled}) {
-    return (
-        <ProjectList enabled={enabled} />
-    );
-}
