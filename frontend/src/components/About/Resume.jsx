fdd92e0ab060b62a7040b0cdb272f141f36a022a
Kavya-Agar <kavyaagar0@gmail.com>
2025-10-29 19:38:50 -0500

    Updated Resume link

--- frontend/src/components/About/Resume.jsx
+++ frontend/src/components/About/Resume.jsx
@@ -1,9 +1,10 @@
 import { GrDocumentUser } from "react-icons/gr";
+import Resume_pdf from "../../assets/KA Resume SWE.pdf";
 
 export const Resume = ({ enabled }) => {
   return (
     <a
-      href="https://drive.google.com/file/d/1yjXL089_K-2ya0a3UqECIs09KQ6Z97tW/view?usp=drive_link"
+      href={Resume_pdf}
       target="_blank"
       rel="noopener noreferrer"
       className={`flex items-center hover:text-amber-400 ${enabled ? "text-amber-400" : "text-amber-100"}`}
